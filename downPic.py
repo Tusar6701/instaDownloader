15e5d07c61354b479e56a8944ae5df524f3a64d8
Tusar6701 <64353205+Tusar6701@users.noreply.github.com>
2021-04-16 11:49:28 +0530

    Small change

--- downPic.py
+++ downPic.py
@@ -4,3 +4,4 @@ ig= instaloader.Instaloader()
 dp= input("Enter the instagram username : ")
 
 ig.download_profile(dp, profile_pic_only=True)
+print("The profile was downloaded successfully, ENJOYYYYY!!!")
